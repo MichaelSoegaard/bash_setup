f2751e95283f15f976ede339f167e92e913c289f
Michael Søegaard <michael@redpost.dk>
2023-01-30 22:15:45 +0100

    setup fixing sudo access

--- setup_python_env.sh
+++ setup_python_env.sh
@@ -5,22 +5,25 @@
 
 echo "Enter project name"
 read project
-echo "Enter python version"
-read python
-curl https://github.com/MichaelSoegaard/bash_setup/blob/master/pyproject.toml
-curl https://github.com/MichaelSoegaard/bash_setup/blob/master/.gitignore
-curl https://github.com/MichaelSoegaard/bash_setup/blob/master/.pre-commit-config.yaml
-curl https://github.com/MichaelSoegaard/bash_setup/blob/master/mypy.ini
-apt-get update
-apt-get install curl -y
+# echo "Enter python version"
+# read python
+curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/pyproject.toml -o ./pyproject.toml
+curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/.gitignore -o ./.gitignore
+curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/.pre-commit-config.yaml -o ./.pre-commit-config.yaml
+curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/mypy.ini -o mypy.ini
+apt-get update < "/dev/null"
+apt-get install curl -y < "/dev/null"
 curl -sSL https://install.python-poetry.org | python3 -
+# echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
 mkdir $project
 mkdir tests
 mkdir LICENSES
-echo "LICENSES/MIT.txt" > LICENSE/MIT.txt
+echo "LICENSES/MIT.txt" > LICENSES/MIT.txt
 touch $project/__init__.py
 touch tests/__init__.py
 touch Readme.md
+poetry --version
 poetry install
 pre-commit install
-poetry pre-commit autoupdate
+
+#newline
