fd579fc29af3b9e34a3a23f5e909dea089009b09
Michael Søegaard <michael@redpost.dk>
2023-01-31 15:40:55 +0100

    Add docstring and export PATH

--- setup_python_env.sh
+++ setup_python_env.sh
@@ -1,20 +1,29 @@
 # SPDX-FileCopyrightText: 2023 Michael Søegaard <https://soegaard.ai>
 # SPDX-License-Identifier: MIT
 
+# Script to setup a new python project with following componenets:
+# - Folders for app, tests and license
+# - setup Poetry for enviroment handling
+# - setup pre-commit hooks
+# - setup MIT licenses
+# Adding pyproject.toml, .gitignore, pre-commit-config.yaml and mypy.ini
+#####################################################################
+# BEFORE RUNNING THIS SCRIPT PLEASE INITIALIZE GIT WITH $ git init
+# ALSO RUN chmod +x setup_python_env.sh
+# to change permissions for this script.
+
 !/usr/bin/bash
 
 echo "Enter project name"
 read project
-# echo "Enter python version"
-# read python
+apt-get update < "/dev/null"
+apt-get install curl -y < "/dev/null"
 curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/pyproject.toml -o ./pyproject.toml
 curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/.gitignore -o ./.gitignore
 curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/.pre-commit-config.yaml -o ./.pre-commit-config.yaml
 curl https://raw.githubusercontent.com/MichaelSoegaard/bash_setup/master/mypy.ini -o mypy.ini
-apt-get update < "/dev/null"
-apt-get install curl -y < "/dev/null"
 curl -sSL https://install.python-poetry.org | python3 -
-# echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
+export PATH="$HOME/.poetry/bin:$PATH"
 mkdir $project
 mkdir tests
 mkdir LICENSES
